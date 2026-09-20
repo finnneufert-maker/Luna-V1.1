d501a3eeb60fb07766c3f1e76d23d12bf8f11e97
finnneufert-maker <finnneufert@gmail.com>
2026-09-20 19:18:09 +0200

    Add Android app configuration

---
+++ app/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "de.luna.assistant"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "de.luna.assistant"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 2
+        versionName = "1.0.1"
+    }
+}
